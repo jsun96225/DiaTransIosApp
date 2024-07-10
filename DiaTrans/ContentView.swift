6661b826734eb804fc62fede13d80a89a5f0e672
jialongsun <jsun96225@gmail.com>
2024-07-10 01:05:45 -0700

    Initial Commit

---
+++ DiaTrans/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiaTrans
+//
+//  Created by Jialong Sun on 7/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiaTrans/DiaTransApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaTransApp.swift
+//  DiaTrans
+//
+//  Created by Jialong Sun on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DiaTransApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
